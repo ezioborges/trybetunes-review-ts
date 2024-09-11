1842be2d2ac924f975666b8c9796be0188e878ce
Ezio Borges <eziopborges@gmail.com>
2024-09-11 08:42:38 -0300

    Ajuste no componente Loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,12 @@
+function Loading() {
+  return (
+    <>
+      <div style={{ height: '100vh' }} className="d-flex flex-column justify-content-center align-items-center">
+        <img src="../../Spinner@1x-1.0s-200px-200px.svg" />
+        <h1>Loading...</h1>
+      </div>
+    </>
+  );
+}
+
+export default Loading;
